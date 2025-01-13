0f314982a7eb82128f11e28d8c4e2dbaec88e20f
Dmytro Gerasymenko <dn190783gda@gmail.com>
2025-01-13 18:09:55 +0200

    [hw02] minor fixes; made 'hw02script.sh' script executable

--- hw02script.sh
+++ hw02script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Please see all users with \"/bin/bash CLI\":"
+echo "Please see all detected users with \"/bin/bash\" CLI below:"
 cat /etc/passwd | grep :/bin/bash$ | awk -F : '{print $1}'
